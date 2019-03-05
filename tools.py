The networks.py module has been significantly changed with the plan of implementing a policy gradient method for an agent's internal state.  Unfortunately, it seems that there is a mistake in the algorithm here which oddly mixes Q-learning with actor-critic methods. A major update is now planned which converts the system to a pure policy gradient method.
The networks at this stage are
as follows.

1) The fixed sensory network
  - This network used to output a flattened vector.  We
  no longer flatten it because it will be read with a
  convolutional network later.

2) Temoral convolutional network
   - A 3d convolutional network that looks at a fixed length
   stack of frames meant to represent the most recent
   few frames that the agent has seen.

3) Field network
  - A Network that reads the internal field state convolutionally
  and then processes it with some hidden layers.

4) Value network
  - This network takes as input the output of networks
  1, 2, and 3 above and outputs value estimates for the
  four movement directions.

5) Internal field policy network
  - This outputs a policy probability distribution for updating
  the internal field.

tools.py
@@ -28,7 +28,7 @@ def show_images(images, cols=1, titles=None):
         a = fig.add_subplot(cols, np.ceil(n_images / float(cols)), n + 1)
         if image.ndim == 2:
             plt.gray()
-        plt.imshow(image,cmap='Greys',vmin=-2.,vmax=2.)
+        plt.imshow(image,cmap='Greys', vmin=-2.,vmax=2.)
         a.set_title(title)
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_images/2)
     plt.show()
@@ -73,53 +73,53 @@ def num_flat_features(state):
 
     return flat_features
 
+#
+# def format_recall(recall):
+#     """
+#     TODO: add this documentation
+#     """
+#     batch_size = len(recall)
+#     #print("formatting recall with lens ")
+#     #print([len(recall[i]["frames"]) for i in range(batch_size)])
+#     action_list = [recall[b]["action"] for b in range(batch_size)]
+#     reward_list = [recall[b]["reward"] for b in range(batch_size)]
+#     not_term_list = [not (recall[b]["terminal"]) for b in
+#                      range(batch_size)]
+#     h_list = [recall[b]["hidden"][0] for b in range(batch_size)]
+#     c_list = [recall[b]["hidden"][1] for b in range(batch_size)]
+#     back_frame_list = [recall[b]["frames"][:-1].squeeze(dim=1) for b in range(batch_size)]
+#     fwd_frame_list = [recall[b]["frames"][-1] for b in range(batch_size)]
+#
+#     actions = torch.tensor(action_list)
+#     rewards = torch.tensor(reward_list)
+#     not_term = torch.tensor(not_term_list)
+#     h = torch.cat(h_list, dim=1)  # batch_size is the second index
+#     c = torch.cat(c_list, dim=1)  #
+#     hidden = (h, c)
+#     #print("back frame lens:  ", [len(back_frame_list[i]) for i in range(batch_size)])
+#     back_frames = torch.nn.utils.rnn.pack_sequence(back_frame_list)
+#     fwd_frames = torch.cat(fwd_frame_list)
+#     output_dict = {
+#         "hidden": hidden,
+#         "back_frames": back_frames,
+#         "forward_frames": fwd_frames,
+#         "actions": actions,
+#         "rewards": rewards,
+#         "not_terminal": not_term
+#     }
+#     return output_dict
 
-def format_recall(recall):
-    """
-    TODO: add this documentation
-    """
-    batch_size = len(recall)
-    #print("formatting recall with lens ")
-    #print([len(recall[i]["frames"]) for i in range(batch_size)])
-    action_list = [recall[b]["action"] for b in range(batch_size)]
-    reward_list = [recall[b]["reward"] for b in range(batch_size)]
-    not_term_list = [not (recall[b]["terminal"]) for b in
-                     range(batch_size)]
-    h_list = [recall[b]["hidden"][0] for b in range(batch_size)]
-    c_list = [recall[b]["hidden"][1] for b in range(batch_size)]
-    back_frame_list = [recall[b]["frames"][:-1].squeeze(dim=1) for b in range(batch_size)]
-    fwd_frame_list = [recall[b]["frames"][-1] for b in range(batch_size)]
-
-    actions = torch.tensor(action_list)
-    rewards = torch.tensor(reward_list)
-    not_term = torch.tensor(not_term_list)
-    h = torch.cat(h_list, dim=1)  # batch_size is the second index
-    c = torch.cat(c_list, dim=1)  #
-    hidden = (h, c)
-    #print("back frame lens:  ", [len(back_frame_list[i]) for i in range(batch_size)])
-    back_frames = torch.nn.utils.rnn.pack_sequence(back_frame_list)
-    fwd_frames = torch.cat(fwd_frame_list)
-    output_dict = {
-        "hidden": hidden,
-        "back_frames": back_frames,
-        "forward_frames": fwd_frames,
-        "actions": actions,
-        "rewards": rewards,
-        "not_terminal": not_term
-    }
-    return output_dict
-
-def scored_permutation(score, x):
-    """
-    score and x are lists with the same length and score must have
-    ordered numerical value (int, float, etc).  Call score[i]
-    the "score of x[i]".  A permuted version of x is returned
-    with elements arranged so that scores go from highest to
-    lowest.
-    """
-
-    if isinstance(score, list):
-        score = torch.tensor(score)
-    (_, perm) = score.sort(descending=True)
-    return [x[perm[i]] for i in range(len(x))]
-
+# def scored_permutation(score, x):
+#     """
+#     score and x are lists with the same length and score must have
+#     ordered numerical value (int, float, etc).  Call score[i]
+#     the "score of x[i]".  A permuted version of x is returned
+#     with elements arranged so that scores go from highest to
+#     lowest.
+#     """
+#
+#     if isinstance(score, list):
+#         score = torch.tensor(score)
+#     (_, perm) = score.sort(descending=True)
+#     return [x[perm[i]] for i in range(len(x))]
+#